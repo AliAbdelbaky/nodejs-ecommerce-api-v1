14bdf01be5d8b2ecab49296d2d33d33bc315a771
@ali_abdebaqy <ali.abdelbaqy@grand-community.com>
2023-03-30 00:20:53 +0200

    create CRUD for categories

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { createCategory, getCategory } = require('../services/category')
+const { createCategory, getCategories, getCategory, updateCategory,deleteCategory } = require('../services/category')
 
-router.route('/').get(getCategory).post(createCategory)
+router.route('/').get(getCategories).post(createCategory)
+router.route('/:id').get(getCategory).put(updateCategory).delete(deleteCategory)
 
 
 module.exports = router 
--- services/category.js
+++ services/category.js
@@ -4,9 +4,23 @@ const slugify = require('slugify')
 const asyncHandler = require('express-async-handler')
 
 
+exports.getCategories = asyncHandler(async (req, res) => {
+    const page = req.query.page * 1 || 1
+    const limit = req.query.limit * 1 || 5
+    const skip = (page - 1) * limit
+
+    const data = await CategoryModel.find({}).skip(skip).limit(limit)
+    res.status(200).json({ total: data.length, page, limit, data })
+})
+
 exports.getCategory = asyncHandler(async (req, res) => {
-    const data = await CategoryModel.find({})
-    res.status(200).json({ total: data.length, data })
+    const { id } = req.params
+    const data = await CategoryModel.findById(id)
+    if (!data) {
+        res.status(404).json({ msg: `No category for this id ${id}` })
+    }
+    res.status(200).json({ data })
+
 })
 
 
@@ -15,4 +29,21 @@ exports.createCategory = asyncHandler(async (req, res) => {
 
     const doc = await CategoryModel.create({ name, slug: slugify(name) })
     res.status(201).json({ data: doc })
+})
+
+exports.updateCategory = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+
+    const data = await CategoryModel.findOneAndUpdate({ _id: id }, { name, slug: slugify(name) }, { new: true })
+    if (!data) {
+        res.status(404).json({ msg: `No category for this id ${id}` })
+    }
+    res.status(200).json({ data })
+})
+
+exports.deleteCategory = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const data = await CategoryModel.findByIdAndDelete(id)
+    res.status(200).json({ msg: 'deleted sucssefully', data })
 })
