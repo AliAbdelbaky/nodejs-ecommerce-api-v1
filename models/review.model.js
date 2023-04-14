5c91a31868b914e6c0125185caa1d42b5ba5fee2
@ali_abdebaqy <ali.abdelbaqy@grand-community.com>
2023-04-14 22:02:18 +0200

    create review model

---
+++ models/review.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: true
+    },
+    ratings: {
+        type: Number,
+        required: true,
+        min: [1, ' Min rating is 1.0'],
+        max: [5, ' Max rating is 5.0']
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, ' Review must belong to user']
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: [true, 'Review must belong to product']
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Review', reviewSchema);
